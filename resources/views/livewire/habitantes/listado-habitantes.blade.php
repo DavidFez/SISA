d42faa6ee1c2905463f0e357c79d5e2e9ed7f4be
David Del Cid <david47.cruz@hotmail.com>
2025-02-13 23:41:25 -0600

    Refactor: Change posiciones de card en modulo de habitantes

--- resources/views/livewire/habitantes/listado-habitantes.blade.php
+++ resources/views/livewire/habitantes/listado-habitantes.blade.php
@@ -1,7 +1,7 @@
 <div class="container mt-5">
     <div class="row g-4 justify-content-center">
 
-        <!-- Listado de Habitantes -->
+        
         <div class="col-md-4">
             <a href="{{ route('opcionListHabitantes') }}" class="text-decoration-none">
                 <div class="card text-center shadow border-0">
@@ -15,21 +15,7 @@
             </a>
         </div>
 
-        <!-- Habitantes Desactivados -->
-        <div class="col-md-4">
-            <a href="#" class="text-decoration-none">
-                <div class="card text-center shadow border-0">
-                    <div class="card-body">
-                        <div class="icon-circle bg-success text-white mb-3 mx-auto d-flex align-items-center justify-content-center" style="width: 70px; height: 70px; border-radius: 50%;">
-                            <i class="fas  fa-user-plus fa-2x"></i>
-                        </div>
-                        <span class="badge text-bg-success fs-5 fw-bold">Nuevo Habitante</span>
-                    </div>
-                </div>
-            </a>
-        </div>
-
-        <!-- Habitantes Por Año -->
+        
         <div class="col-md-4">
             <a href="{{ route('opcionListNoActivos') }}" class="text-decoration-none">
                 <div class="card text-center shadow border-0">
@@ -38,6 +24,20 @@
                             <i class="fas fa-user-slash fa-2x"></i>
                         </div>
                         <span class="badge text-bg-danger fs-5 fw-bold">Habitantes No Activos</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+    
+        <div class="col-md-4">
+            <a href="#" class="text-decoration-none">
+                <div class="card text-center shadow border-0">
+                    <div class="card-body">
+                        <div class="icon-circle bg-success text-white mb-3 mx-auto d-flex align-items-center justify-content-center" style="width: 70px; height: 70px; border-radius: 50%;">
+                            <i class="fas  fa-user-plus fa-2x"></i>
+                        </div>
+                        <span class="badge text-bg-success fs-5 fw-bold">Nuevo Habitante</span>
                     </div>
                 </div>
             </a>
