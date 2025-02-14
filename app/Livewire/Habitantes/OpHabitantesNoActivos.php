5b918c80f11082d87ea8ec8d9b78e7de6abe690e
David Del Cid <david47.cruz@hotmail.com>
2025-02-13 23:51:39 -0600

    Docs: Add comentarios de funcionabilidad en componentes de habitantes

--- app/Livewire/Habitantes/OpHabitantesNoActivos.php
+++ app/Livewire/Habitantes/OpHabitantesNoActivos.php
@@ -13,6 +13,11 @@ class OpHabitantesNoActivos extends Component
 
     public function render()
     {
+        /*  En esta parte se hace una consulta para obtner todos los habitantes que no esten activos, se usa el operador de 
+            negacion ya que la ideas es obtner todos aquellos estados distintos a Activo, y poder mostrarlo con una etiqueta 
+            diferente en la vista del blade
+        */
+        
         $habitantesNoActivos = Habitante::where('estado', '!=', 'Activo')->paginate(10);
         return view('livewire.habitantes.op-habitantes-no-activos', compact('habitantesNoActivos'));
     }
--- app/Livewire/Habitantes/OpListadoHabitantes.php
+++ app/Livewire/Habitantes/OpListadoHabitantes.php
@@ -14,8 +14,14 @@ class OpListadoHabitantes extends Component
 
     public function render()
     {
+        /*  Para esta seccion obtenemos todos aquellos habitantes que estan activos, es decir que residen en la comunidad y que 
+            actualmente se les brinda un servicio de salud. Para ello el verificamos el campo estado, este campo en un futuro 
+            tendria que estar normalizado y que los estados esten en una tabla diferente en la DB para que sea más escalable y solo
+            se verifique el id del campo y no todo el sting como se hace actualmente, la propiedad paginate, permite mostrar
+            los registros de 10 en 10
+        */
+        
         $habitantes = Habitante::where('estado', 'Activo')->paginate(10);
-
         return view('livewire.habitantes.op-listado-habitantes', compact('habitantes'));
     }
 }
